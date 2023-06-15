d90cbd1318089c62de93b546b0b9de8032fd2f51
hyunjoon125 <133008050+hyunjoon125@users.noreply.github.com>
2023-06-15 12:01:06 +0000

    오현

--- sticky_memo/memo/views.py
+++ sticky_memo/memo/views.py
@@ -1,12 +1,32 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect
 from memo.models import Memo
+from memo.forms import MemoForm
+
 
 # Create your views here.
 # sticky_memo/memo/views.py
 
 def get_all_memo(request):
     memo_list = Memo.objects.all()
-    
     context = {"memos": memo_list}
 
     return render(request, "memo_list.html", context)
+
+def get_memo(request, pk):
+    memo = Memo.objects.get(id=pk)
+    context = {"memo": memo } 
+
+    return render(request, "memo_detail.html", context)
+
+
+def create_memo(request):
+    if request.method == 'GET':
+        form = MemoForm(request.POST)
+        context= {'form': form}
+        return render(request, 'memo_create.html', context)
+    
+    elif request.method == 'POST':
+        form = MemoForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('memo_list')
